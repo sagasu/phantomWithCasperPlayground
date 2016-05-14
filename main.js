3c7a3a559dfd36bcf2f6a511aef03cbbfbadde15
sagasu <sagasu@gmail.com>
2016-05-14 20:07:12 +0100

    capturing as picture websites

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 var casper = require("casper").create();
-console.log("hello world");
-casper.exit();
+casper.start("http://www.google.com", function () {
+    this.capture('./output/google.png')
+})
+
+casper.thenOpen("http://bing.com", function () {
+    this.capture("./output/bing.png")
+})
+
+// at the end of run it automatically calls exit()
+casper.run();
