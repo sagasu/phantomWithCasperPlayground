3b5381046ef591f610e77e5521912b1bf4f46dc0
sagasu <sagasu@gmail.com>
2016-05-14 20:31:09 +0100

    playing with a script and a page context

--- evaluate.js
+++ evaluate.js
@@ -1,10 +1,24 @@
 var casper = require("casper").create();
 casper.start("http://www.google.com", function () {
+    var message = "I am a script context"
+    // this is a callback function that will be executed in a context of a remote webpage
+    // that's why it passing a value such as message doesn't work
     var title = this.evaluate(function () {
         var title = document.title
-        return title
+
+        // this will return null, because message is in script context, while here we are in a page context
+        // remove message to nicely return the title.
+        return title + message
     })
     console.log(title)
+
+    // Here we are passing message to a callback function
+    title = this.evaluate(function (message) {
+        var title = document.title
+        return title + message
+    }, message)
+    console.log(title)
+
 })
 
 casper.run();
